9895da91ac55746fc4dd25855cf765645ec98eb3
Dhairya Gandhi <dhairya@juliacopmuting.com>
2020-10-08 19:15:32 +0530

    add exports

--- Heise.jl
+++ Heise.jl
@@ -10,6 +10,9 @@ using Pkg; Pkg.activate("/Users/dhairyagandhi/Downloads/temp/heise")
 # ╔═╡ da3498b2-0949-11eb-334c-797cbc669918
 using Metalhead, Flux
 
+# ╔═╡ 7577c812-095b-11eb-398f-3105e66097d2
+using Flux3D, Zygote, AbstractPlotting, Statistics
+
 # ╔═╡ 5ae1b5c4-094e-11eb-3acc-7b0274b783de
 using DifferentialEquations, Plots
 
@@ -17,10 +20,10 @@ using DifferentialEquations, Plots
 md"# Metalhead Example"
 
 # ╔═╡ eaa00c4e-0949-11eb-0a3c-b7f2c42a2fa0
-begin
-	base_path = "/Users/dhairyagandhi/Downloads/temp/heise"
-	philip = load(joinpath(base_path, "philip_crop.jpg"))
-end
+base_path = "/Users/dhairyagandhi/Downloads/temp/heise/data"
+
+# ╔═╡ 036e7826-0968-11eb-0057-7da246940506
+philip = load(joinpath(base_path, "philip_crop.jpg"))
 
 # ╔═╡ b780087a-094a-11eb-1a1f-5b55312cb4de
 vgg = VGG19()
@@ -40,8 +43,11 @@ begin
 	_offset = zeros(Float32, size(get_verts_packed(src))...)
 end
 
+# ╔═╡ 98f4f9e8-0967-11eb-216a-fbfb0e35a36b
+# using GLMakie
+
 # ╔═╡ d01902fc-095d-11eb-1543-7f23f03befe2
-save("initial_guess.png", visualize(src));
+# save(joinpath(base_path, "initial_guess2.png"), visualize(src))
 
 # ╔═╡ 6449dd0e-095c-11eb-27ab-3b8d91e6617d
 md"""
@@ -78,6 +84,12 @@ md"""
 ## Training the Model
 """
 
+# ╔═╡ e012f584-096a-11eb-3e6b-d3be7a19a19b
+save(joinpath(base_path, "somewhat.png"), visualize(Flux3D.offset(src, _offset)))
+
+# ╔═╡ 26687fb8-096b-11eb-25ee-7da0e0d908f9
+save(joinpath(base_path, "somewhat.png"), visualize(Flux3D.offset(src, _offset)))
+
 # ╔═╡ 06ffc29e-094d-11eb-17ef-07d69ac6cbd3
 md"# SciML Example - Hooke's Law"
 
@@ -98,7 +110,7 @@ begin
 	force(dx,x,k,t) = -k*x + 0.1sin(x)
 	prob = SecondOrderODEProblem(force,1.0,0.0,(0.0,10.0),k)
 	sol = solve(prob)
-	plot(sol,label=["Velocity" "Position"])
+	Plots.plot(sol,label=["Velocity" "Position"])
 end
 
 # ╔═╡ 73d23cc4-0954-11eb-2b72-9d4a380c8474
@@ -120,13 +132,13 @@ begin
 	position_data = [state[2] for state in sol(t)]
 	force_data = [force(state[1],state[2],k,t) for state in sol(t)]
 
-	plot(plot_t,force_plot,xlabel="t",label="True Force")
-	scatter!(t,force_data,label="Force Measurements")
+	Plots.plot(plot_t,force_plot,xlabel="t",label="True Force")
+	Plots.scatter!(t,force_data,label="Force Measurements")
 end
 
 # ╔═╡ 86f2b826-0954-11eb-0c5e-27345574f616
 md"""
-## Generate Neural Network and L2 loss
+## Define Neural Network and L2 Loss
 
 The neural network is trained to match the force values at every position
 """
@@ -153,6 +165,18 @@ begin
 	Flux.train!(loss, Flux.params(NNForce), data, opt)
 end
 
+# ╔═╡ b5293b88-0969-11eb-32b0-db67331d7201
+begin
+	θ = Flux.params(_offset)
+	nepochs = 10
+	for itr in 1:nepochs
+		gs = gradient(θ) do
+			loss_dolphin(_offset, src, tgt)
+		end
+		Flux.update!(opt, _offset, gs[_offset])
+	end
+end
+
 # ╔═╡ 9e31f3f6-0954-11eb-135d-69b7d06406b7
 md"""
 ## Plot - Trained Model with 4 Datapoints
@@ -164,9 +188,9 @@ The trained model fits the datapoints perfectly, but does not capture the real p
 begin
 	learned_force_plot = NNForce.(positions_plot)
 
-	plot(plot_t,force_plot,xlabel="t",label="True Force")
-	plot!(plot_t,learned_force_plot,label="Predicted Force")
-	scatter!(t,force_data,label="Force Measurements")
+	Plots.plot(plot_t,force_plot,xlabel="t",label="True Force")
+	Plots.plot!(plot_t,learned_force_plot,label="Predicted Force")
+	Plots.scatter!(t,force_data,label="Force Measurements")
 end
 
 # ╔═╡ 7e17f830-0955-11eb-0c30-a3f05c0b5811
@@ -186,8 +210,8 @@ begin
 	force2(dx,x,k,t) = -k*x
 	prob_simplified = SecondOrderODEProblem(force2,1.0,0.0,(0.0,10.0),k)
 	sol_simplified = solve(prob_simplified)
-	plot(sol,label=["Velocity - Real" "Position - Real"])
-	plot!(sol_simplified,label=["Velocity - Ideal" "Position - Ideal"])
+	Plots.plot(sol,label=["Velocity - Real" "Position - Real"])
+	Plots.plot!(sol_simplified,label=["Velocity - Ideal" "Position - Ideal"])
 end
 
 # ╔═╡ e311d2a6-0955-11eb-084b-c99c3034d928
@@ -226,9 +250,9 @@ md"""
 let
 	learned_force_plot = NNForce.(positions_plot)
 
-	plot(plot_t,force_plot,xlabel="t",label="True Force")
-	plot!(plot_t,learned_force_plot,label="Predicted Force")
-	scatter!(t,force_data,label="Force Measurements")
+	Plots.plot(plot_t,force_plot,xlabel="t",label="True Force")
+	Plots.plot!(plot_t,learned_force_plot,label="Predicted Force")
+	Plots.scatter!(t,force_data,label="Force Measurements")
 end
 
 # ╔═╡ 48c3dc52-095b-11eb-1b64-af6d28273fd2
@@ -236,29 +260,27 @@ md"""
 This shows the trained neural network approximating the actual force function very closely.
 """
 
-# ╔═╡ 7577c812-095b-11eb-398f-3105e66097d2
-using Flux3D, AbstractPlotting, GLMakie, Statistics
-
-# ╔═╡ f1f7e1c8-095c-11eb-36c0-bf6468b09a41
-using GLMakie
-
 # ╔═╡ Cell order:
 # ╠═b7961f1c-0949-11eb-248e-c32b842623f9
 # ╟─513387ec-094d-11eb-0c0d-191d7d29324e
-# ╠═da3498b2-0949-11eb-334c-797cbc669918
 # ╠═eaa00c4e-0949-11eb-0a3c-b7f2c42a2fa0
+# ╠═da3498b2-0949-11eb-334c-797cbc669918
+# ╠═036e7826-0968-11eb-0057-7da246940506
 # ╠═b780087a-094a-11eb-1a1f-5b55312cb4de
 # ╠═0d775986-094b-11eb-2ad7-9bb105303ff3
 # ╟─8f3029e2-0952-11eb-019d-f970521a289d
 # ╠═7577c812-095b-11eb-398f-3105e66097d2
 # ╠═927b1632-095b-11eb-2dad-ddb3af677929
-# ╠═f1f7e1c8-095c-11eb-36c0-bf6468b09a41
+# ╠═98f4f9e8-0967-11eb-216a-fbfb0e35a36b
 # ╠═d01902fc-095d-11eb-1543-7f23f03befe2
 # ╟─6449dd0e-095c-11eb-27ab-3b8d91e6617d
 # ╠═f03869bc-095b-11eb-3df8-f5ba609da21b
 # ╟─86f1c9b6-095c-11eb-2d83-717eb77f496d
 # ╠═c644cfc4-095b-11eb-2802-bb397ea7c651
 # ╟─d7fe6b30-095b-11eb-0de2-735b8f01fae7
+# ╠═b5293b88-0969-11eb-32b0-db67331d7201
+# ╠═e012f584-096a-11eb-3e6b-d3be7a19a19b
+# ╠═26687fb8-096b-11eb-25ee-7da0e0d908f9
 # ╟─06ffc29e-094d-11eb-17ef-07d69ac6cbd3
 # ╟─73830ad4-094d-11eb-1047-4760599caa35
 # ╠═5ae1b5c4-094e-11eb-3acc-7b0274b783de
@@ -266,7 +288,7 @@ using GLMakie
 # ╠═faf906d2-0950-11eb-2a5b-a316dc540d47
 # ╟─73d23cc4-0954-11eb-2b72-9d4a380c8474
 # ╠═2b7e03ba-0952-11eb-35b1-4b9558e06b26
-# ╠═86f2b826-0954-11eb-0c5e-27345574f616
+# ╟─86f2b826-0954-11eb-0c5e-27345574f616
 # ╠═5d3786a8-0952-11eb-3ac6-fd4f0a601ef9
 # ╟─6f581854-0955-11eb-35db-99edd4af6d06
 # ╠═cf7392c8-0952-11eb-38b5-83798ad179f1
@@ -274,7 +296,7 @@ using GLMakie
 # ╠═74a5d4ae-0953-11eb-15ec-2de8ce4a95ae
 # ╟─7e17f830-0955-11eb-0c30-a3f05c0b5811
 # ╟─2763ea68-0954-11eb-25bc-1960797a6aff
-# ╟─9eed0ebc-0953-11eb-2f1b-795859d1bfd8
+# ╠═9eed0ebc-0953-11eb-2f1b-795859d1bfd8
 # ╟─e311d2a6-0955-11eb-084b-c99c3034d928
 # ╠═b6989dd6-0955-11eb-0675-c5f03090aadc
 # ╟─9807880e-0956-11eb-1b79-9b400845072b
